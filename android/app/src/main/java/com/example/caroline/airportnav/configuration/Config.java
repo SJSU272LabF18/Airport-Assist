6b555953356c6ba0d503bdcd11762de851a81c99
Caroline-Rajah <caroline9005@gmail.com>
2018-11-27 04:25:45 -0800

    Update Config.java

--- android/app/src/main/java/com/example/caroline/airportnav/configuration/Config.java
+++ android/app/src/main/java/com/example/caroline/airportnav/configuration/Config.java
@@ -2,7 +2,7 @@ package com.example.caroline.airportnav.configuration;
 
 public class Config {
 
-    public static final String ACCESS_TOKEN = "881a5d9e43824ad7ab3ec66b0ae27f8d";
+    public static final String ACCESS_TOKEN = "";
 
 
 }
